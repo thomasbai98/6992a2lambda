c0f1b27f0cd8fc5f91b445bd59bfb4bf3f4444fc
Yuyang Bai <yb2519@columbia.edu>
2022-11-21 14:50:50 -0500

    Add files via upload

--- search-photos/lambda_function.py
+++ search-photos/lambda_function.py
@@ -7,6 +7,7 @@ from requests_aws4auth import AWS4Auth
 client = boto3.client('lexv2-runtime')
 def lambda_handler(event, context):
     # TODO implement
+    print("something visible")
     print("events:",event)
     msg_from_user = event["q"]
     id = str(hash(str(time.time())))
